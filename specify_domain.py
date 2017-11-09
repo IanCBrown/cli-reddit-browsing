bcc126fb3919c6973168bf24210678811f5addb1
Ian Brown <icb0004@auburn.edu>
2017-11-09 16:28:47 -0600

    ability to choose domains other than gfycat now implemented

--- specify_domain.py
+++ specify_domain.py
@@ -5,23 +5,48 @@ import sys
 
 # TODO give the user the ability to specify domain (youtube, imgur, etc), not just 
 # gfycat
-def get_webms(subreddit):
-    reddit = praw.Reddit(client_id='LJLwIfxLzSjbug', 
+reddit = praw.Reddit(client_id='LJLwIfxLzSjbug', 
                         client_secret='_c_TeQKNiHjTnIIqpgLbI4Fc8Kg', 
                         user_agent='retrieve_webms_from_gfycat')
+def domains(user_input):
+    # Using a dictionary as a switch for cli options
+    # Future functions will be added here 
+    try:
+        actions = { 'g' : ['gfycat.com'],
+                    'y' : ['youtube.com', 'youtu.be'],
+                    'i' : ['imgur.com', 'i.imgur.com'],
+                    's' : ['streamable.com'] }
+        return actions[user_input]
+    except KeyError:
+        print("Command not found")
+  
+def get_posts_from_domains(subreddit, domain):
     try:
         subreddit = reddit.subreddit(subreddit)
+        i = 1
         for submission in subreddit.hot(limit=30):
-            if submission.domain == 'gfycat.com':
-                print(submission.title + "\n" + submission.url)
+            if submission.domain == domain[0] or submission.domain == domain[1]:
+                print("{}. ".format(i) + submission.title) 
+                print('\t' + submission.shortlink)
+                print('\t' + submission.url + '\n')
+                i += 1
     except prawcore.exceptions.NotFound:
         print("Subreddit not found")
 
-
+def get_domains(subreddit):
+    try: 
+        subreddit = reddit.subreddit(subreddit)
+        
+        for submission in subreddit.hot(limit=30):
+            print(submission.domain)
+    except prawcore.exceptions.NotFound:
+        print("Subreddit not found")
 
 def main():
     subreddit = input("Input a subreddit: ")
-    get_webms(subreddit)
+    domain = input("Choose a domain:\n[G] - gfycat\t[Y] - youtube\t[I] - imgur\t[S] - streamble\n")
+
+    get_posts_from_domains(subreddit, domains(domain))
 
 
 if __name__ == '__main__':
